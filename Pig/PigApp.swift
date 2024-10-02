44a4f468b37ce082cf19643242583a22157a57a9
Nolan <nolanbm2009@icloud.com>
2024-10-01 19:46:51 -0500

    Initial Commit

---
+++ Pig/PigApp.swift
@@ -0,0 +1,17 @@
+//
+//  PigApp.swift
+//  Pig
+//
+//  Created by Nolan Brian McSweeney on 10/1/24.
+//
+
+import SwiftUI
+
+@main
+struct PigApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PigTests/PigTests.swift
@@ -0,0 +1,17 @@
+//
+//  PigTests.swift
+//  PigTests
+//
+//  Created by Nolan Brian McSweeney on 10/1/24.
+//
+
+import Testing
+@testable import Pig
+
+struct PigTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
